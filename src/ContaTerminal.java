a319ba52964389df205a29c948b6f68224fde650
dan7sc <dan7sc@users.noreply.github.com>
2024-08-09 15:05:58 -0300

    iniciando o projeto conta banco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String... args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner
+
+        // TODO: Exibir as mensagens para o nosso usuario
+
+        // TODO: Obter pela scanner os valores digitados no terminal
+
+        // TODO: Exibir a mensagem conta criada
+
+        System.out.println("Hello, world!");
+    }
+}
