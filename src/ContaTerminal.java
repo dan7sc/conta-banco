36776634f0add6d3b35f43c3b43dd8e449672ecb
dan7sc <dan7sc@users.noreply.github.com>
2024-08-10 16:55:15 -0300

    implementando projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,13 +1,73 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String... args) throws Exception {
-        // TODO: Conhecer e importar a classe Scanner
+        int numeroDaConta = 0;
+        String numeroDaAgencia = "";
+        String nomeDoCliente = "";
+        double saldo = 0.00;
+        boolean insercaoValida = false;
 
-        // TODO: Exibir as mensagens para o nosso usuario
+        Scanner scanner = new Scanner(System.in);
 
-        // TODO: Obter pela scanner os valores digitados no terminal
+        do {
+            try {
+                System.out.print("Por favor, insira o numero da conta: ");
+                numeroDaConta = scanner.nextInt();
+                insercaoValida = true;
+            } catch (Exception e) {
+                scanner.next();
+                System.out.println("Valor invalido. Digite um numero inteiro.");
+            }
+        } while (!insercaoValida);
 
-        // TODO: Exibir a mensagem conta criada
+        insercaoValida = false;
+        do {
+            try {
+                System.out.print("Por favor, insira o numero da agencia: ");
+                numeroDaAgencia = scanner.next();
+                if (numeroDaAgencia.length() != 4) {
+                    throw new IllegalArgumentException();
+                }
+                insercaoValida = true;
+                numeroDaAgencia = numeroDaAgencia.substring(0, 3) + "-" + numeroDaAgencia.substring(3);
+            } catch (Exception e) {
+                System.out.println("Valor invalido. Insira uma conta com 4 digitos.");
+            }
+        } while (!insercaoValida);
 
-        System.out.println("Hello, world!");
+        insercaoValida = false;
+        while (!insercaoValida) {
+            try {
+                System.out.print("Por favor, insira o nome do cliente: ");
+                nomeDoCliente = scanner.next();
+                if (nomeDoCliente.length() <= 2) {
+                    throw new IllegalArgumentException();
+                }
+                insercaoValida = true;
+            } catch (Exception e) {
+                System.out.println("Valor invalido. Nome dever ter mais de 2 caracteres.");
+            }
+        }
+
+        insercaoValida = false;
+        while (!insercaoValida) {
+            try {
+                System.out.print("Por favor, insira o valor do saldo: ");
+                saldo = scanner.nextDouble();
+                insercaoValida = true;
+            } catch (Exception e) {
+                scanner.next();
+                System.out.println("Valor invalido. Insira um numero.");
+            }
+        }
+
+        scanner.close();
+
+        final String mensagem = "Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + numeroDaAgencia + ", conta " + numeroDaConta + " e seu saldo " +
+                String.format("%.2f", saldo ) + " já está disponível para saque";
+        System.out.println(mensagem);
+
     }
 }
